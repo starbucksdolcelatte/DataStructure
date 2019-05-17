61f30ee3e11234e15820e6692e3a808b56ec0fdb
starbucksdolcelatte <seoyoon9535@gmail.com>
2019-05-17 13:55:49 +0900

    Stack by Linked List

---
+++ [week8]Stack_Queue_by_LinkedList/stack_linked_list.h
@@ -0,0 +1,18 @@
+#pragma once
+#include <stdio.h> 
+#define TRUE 1 
+#define FALSE 0
+
+typedef struct _Node{ 
+	int data; 
+	struct _Node* next; 
+}Node; 
+
+typedef struct _ListStack{ 
+	struct _Node* head; 
+}ListStack;
+
+void StackInit(ListStack * pstack);
+int SIsEmpty(ListStack * pstack);
+void SPush(ListStack * pstack, int data);
+int SPop(ListStack * pstack);
