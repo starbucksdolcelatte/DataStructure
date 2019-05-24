84e9e01ff5bdfec3bbf0f171c22a406db6f5e938
starbucksdolcelatte <seoyoon9535@gmail.com>
2019-05-25 03:44:03 +0900

    Binary Tree

---
+++ [week9]Binary_Search_Tree/binaryTree.h
@@ -0,0 +1,18 @@
+#pragma once
+#include <stdio.h> 
+#define TRUE 1 
+#define FALSE 0
+
+typedef struct _BTreeNode {
+	int data;
+	struct BTreeNode* left;
+	struct BTreeNode* right;
+}BTreeNode;
+
+BTreeNode* MakeBTreeNode(void);
+int GetData(BTreeNode* bt);
+void SetData(BTreeNode* bt, int data);
+BTreeNode* GetLeftSubTree(BTreeNode* bt);
+BTreeNode* GetRightSubTree(BTreeNode* bt);
+void MakeLeftSubTree(BTreeNode* main, BTreeNode* sub);
+void MakeRightSubTree(BTreeNode* main, BTreeNode* sub);
